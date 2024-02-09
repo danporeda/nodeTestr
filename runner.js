0fb12125fbeec56e0c87b1eef34e02203f9debac
Dan <danporeda@yahoo.com>
2024-02-09 14:50:53 -0500

    refactored global.it with try-catch for test result messages

--- runner.js
+++ runner.js
@@ -6,6 +6,28 @@ class Runner {
     this.testFiles = [];
   }
 
+  async runTests() {
+    for (let file of this.testFiles) {
+      const beforeEaches = [];
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+
+      global.it = (desc, fn) => {
+        beforeEaches.forEach(func => func());
+        try {
+          fn();
+          console.log(`OK - ${desc}`);
+        } catch (err) {
+          console.log(`FAILED - ${desc}`)
+          console.log(err);
+        }
+      };
+
+      require(file.name);
+    }
+  }
+
   async collectFiles(targetPath) {
     const files = await fs.promises.readdir(targetPath);
 
