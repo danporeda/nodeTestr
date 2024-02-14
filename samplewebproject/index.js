91101cfa7d77adbeeff45bd081d106008fe0548a
Dan <danporeda@yahoo.com>
2024-02-14 11:35:11 -0800

    added evaluation logic to email input in /samplewebproject

--- samplewebproject/index.js
+++ samplewebproject/index.js
@@ -4,7 +4,7 @@ document.querySelector('form').addEventListener('submit', (event) => {
   const { value } = document.querySelector('input');
 
   const header = document.querySelector('h1');
-  if (value.includes('@')) {
+  if (value.includes('@') && value.length >= 5) {
     header.innerHTML = 'Looks good!';
   } else {
     header.innerHTML = 'invalid email';
